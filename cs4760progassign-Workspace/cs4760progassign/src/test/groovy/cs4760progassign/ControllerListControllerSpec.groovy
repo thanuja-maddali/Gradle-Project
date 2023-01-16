f460fbf84d78a948010fd5f9e982ca1701274b9d
thanujamaddali <thanujamaddali1132@gmail.com>
2023-01-16 15:35:41 -0500

    Initial Commit

---
+++ cs4760progassign-Workspace/cs4760progassign/src/test/groovy/cs4760progassign/ControllerListControllerSpec.groovy
@@ -0,0 +1,18 @@
+package cs4760progassign
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class ControllerListControllerSpec extends Specification implements ControllerUnitTest<ControllerListController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
